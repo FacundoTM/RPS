320fd9d81586360fa38fbdf71b642734d5523c15
FacundoTM <tmfacundo@gmail.com>
2022-08-03 13:46:46 -0300

    Style changes

--- rps.js
+++ rps.js
@@ -8,16 +8,21 @@ let computerScore = 0;
 let playerScore = 0;
 let r = document.getElementById('result');
 let c = document.getElementById('choice');
+let body = document.querySelector("body");
 
 function check(){
 
     if (playerScore >= 5) {
-        c.innerHTML = "<img src='./player.png'>"
-        r.innerHTML = `<h1> <strong>END!!</strong> player wins!! <br></h1> <button style="width: 100px;margin: 0 auto;" onclick = "location.reload();">Restart</button> `;
+        c.innerHTML = "<img src='./end.png'>"
+        r.innerHTML = `<h1> <strong>END!!</strong> player wins!! <br></h1> <button style="width: 450px;height:130px;margin: 0 auto;font-size:48px;" onclick = "location.reload();">RESTART</button> 
+        <br><br>FINAL SCORE <br> PC: ${computerScore},   PLAYER: ${playerScore}`;
+        body.setAttribute("style","background-color:#28b4e5;color:white;font-family: Helvetica;text-align: center;display: flex;flex-flow: column;height: 1080px;justify-content: center;");
         playerScore = computerScore = 0;
     } else if (computerScore >= 5) {
-        c.innerHTML = "<img src='./pc.webp'>";
-        r.innerHTML = `<h1> <strong>END!!</strong> computer wins!!<br></h1> <button style="width: 100px;margin: 0 auto;" onclick = "location.reload();">Restart</button>`;
+        c.innerHTML = "<img src='./end.png'>";
+        r.innerHTML = `<h1> <strong>END!!</strong> computer wins!!<br></h1> <button style="width: 450px;height:130px;margin: 0 auto;font-size:48px;" onclick = "location.reload();">RESTART</button>
+        <br><br>FINAL SCORE <br> PC: ${computerScore},   PLAYER: ${playerScore}`;
+        body.addAttribute("style","background-color:#28b4e5;color:white;font-family: Helvetica;text-align: center;display: flex;flex-flow: column;height: 1080px;justify-content: center;");
         playerScore = computerScore = 0;
     }
 }
@@ -25,15 +30,15 @@ function check(){
 function rock() {
     switch(computerPlay()) {
         case 'rock':
-            r.innerHTML = 'tie';
+            r.innerHTML = `tie <br> computer: ${computerScore}|  player: ${playerScore}`;
             break;
         case 'paper':
             computerScore++;
-            r.innerHTML = 'pc wins';
+            r.innerHTML = `pc wins <br> computer: ${computerScore}|  player: ${playerScore}`;
             break;
         case 'scissors':
             playerScore++;
-            r.innerHTML = 'player win';
+            r.innerHTML = `player win <br> computer: ${computerScore}|  player: ${playerScore}`;
             break;
     }
     check();
@@ -43,15 +48,15 @@ function rock() {
 function paper() {
     switch(computerPlay()) {
         case 'rock':
-            r.innerHTML ='player wins';
             playerScore++;
+            r.innerHTML =`player win <br> computer: ${computerScore}|  player: ${playerScore}`;
             break;
         case 'paper':
-            r.innerHTML ='tie';
+            r.innerHTML =`tie <br> computer: ${computerScore}|  player: ${playerScore}`;
             break;
         case 'scissors':
-            r.innerHTML ='pc win';
             computerScore++;
+            r.innerHTML =`pc wins <br> computer: ${computerScore}|  player: ${playerScore}`;
             break;
     }
     check();
@@ -61,15 +66,15 @@ function paper() {
 function scissors() {
     switch(computerPlay()) {
         case 'rock':
-            r.innerHTML ='pc wins';
             computerScore++;
+            r.innerHTML =`pc wins <br> computer: ${computerScore}|  player: ${playerScore}`;
             break;
         case 'paper':
-            r.innerHTML ='player wins';
             playerScore++;
+            r.innerHTML =`player win <br> computer: ${computerScore}|  player: ${playerScore}`;
             break;
         case 'scissors':
-            r.innerHTML ='tie';
+            r.innerHTML =`tie <br> computer: ${computerScore}|  player: ${playerScore}`;
             break;
     }
     check();
